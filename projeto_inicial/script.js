5a267ca6e8bfea7c22bac9e834e2a060d1028cd8
Robert <112430289+robertvieiraa@users.noreply.github.com>
2026-02-18 20:18:34 -0300

    Adicionei botão de concluir tarefa

--- projeto_inicial/script.js
+++ projeto_inicial/script.js
@@ -7,8 +7,18 @@ const criarTarefa = (evento) => {
     tarefa.classList.add('task')
     const conteudo = `<p class="content">${valor}</p>`
     tarefa.innerHTML = conteudo
+    tarefa.appendChild(BotaoConclui())
     lista.appendChild(tarefa)
     input.value = " "
 }
 const novaTarefa = document.querySelector('[data-form-button]')
 novaTarefa.addEventListener('click', criarTarefa)
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button')
+    botaoConclui.classList.add('check-button')
+    botaoConclui.innerText = 'Concluir'
+    botaoConclui.addEventListener('click', () => {
+        console.log('fui clicado')
+    })
+    return botaoConclui
+}
