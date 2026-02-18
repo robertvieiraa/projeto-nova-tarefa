ee87225f00b6fdd9bb2a2e7dbdc212a8b794e034
Robert <112430289+robertvieiraa@users.noreply.github.com>
2026-02-18 16:56:11 -0300

    Código organizado

--- projeto_inicial/script.js
+++ projeto_inicial/script.js
@@ -1,6 +1,9 @@
-const novaTarefa = document.querySelector('[data-form-button]')
-novaTarefa.addEventListener('click', () => {
+const criarTarefa = (evento) => {
+    evento.preventDefault()
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
     console.log(valor)
-})
+    input.value = " "
+}
+const novaTarefa = document.querySelector('[data-form-button]')
+novaTarefa.addEventListener('click', criarTarefa)
