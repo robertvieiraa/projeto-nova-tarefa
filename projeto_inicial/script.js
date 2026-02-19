4e50e24d5f647973a58e3355c5cdb50f53d58626
Robert <112430289+robertvieiraa@users.noreply.github.com>
2026-02-19 10:15:08 -0300

    Adicionei o botão de deletar tarefa

--- projeto_inicial/script.js
+++ projeto_inicial/script.js
@@ -9,6 +9,7 @@ const criarTarefa = (evento) => {
     const conteudo = `<p class="content">${valor}</p>`
     tarefa.innerHTML = conteudo
     tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoDeleta())
     lista.appendChild(tarefa)
     input.value = " "
 }
@@ -21,11 +22,24 @@ const BotaoConclui = () => {
     botaoConclui.innerText = 'Concluir'
     botaoConclui.addEventListener('click', concluirTarefa)
     return botaoConclui
-}
+}// componente
 
 const concluirTarefa = (evento) => {
     const botaoConclui = evento.target
     const tarefaCompleta = botaoConclui.parentElement
     tarefaCompleta.classList.toggle('done')
+}
+
+const BotaoDeleta = () => {
+    const botaoDeleta = document.createElement('button')
+    botaoDeleta.innerText = `Deletar`
+    botaoDeleta.addEventListener('click', deletarTarefa)
+    return botaoDeleta
+}
+
+const deletarTarefa = (evento) => {
+    const botaoDeleta = evento.target
+    const tarefaCompleta = botaoDeleta.parentElement
+    tarefaCompleta.remove()
 }
 })()
